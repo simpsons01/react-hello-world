2e6c19e9f0c0fd1ca5beb9042623e040eb78e0d8
ilikekobe01@gmail.com <ilikekobe01@gmail.com>
2022-04-21 18:22:41 +0800

    add counter

--- src/index.js
+++ src/index.js
@@ -2,4 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>hello world</h1>);
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+
+  add() {
+    this.setState(prevState => ({ count: prevState.count + 1 }))
+  }
+
+  render() {
+    return (
+      <div>
+        <div>{this.state.count}</div>
+        <button onClick={this.add.bind(this)}>+</button>
+      </div>
+    )
+  }
+}
+root.render(<Counter />);
