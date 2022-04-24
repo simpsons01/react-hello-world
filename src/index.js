ef53d4ae7d051bbbfa4b52055e12c351a2570094
simpsons01 <ilikekobe01@gmail.com>
2022-04-24 23:29:12 +0800

    test componentUpdate

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+class Counter extends React.Component {
+  
+  componentDidUpdate() {
+    console.log("componnet did update")
+  }
+
+  render() {
+    console.log("componet did render")
+    return (
+      <div>
+        {this.props.count}
+      </div>
+    )
+  }
+}
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 1
+    }
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.setState((prevState) => ({ count: prevState.count + 1 }))}>+</button>
+        <Counter 
+           count={this.state.count}
+        />
+      </div>
+    )
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>hello world</h1>);
+root.render(<App />);
