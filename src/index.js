42845eb23731193c951168a7e2a6fe8c009e758a
simpsons01 <ilikekobe01@gmail.com>
2022-04-24 16:39:49 +0800

    use ref

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+class Input extends React.Component {
+   constructor(props) {
+     super(props)
+     this.inputRef = React.createRef()
+   }
+
+   componentDidMount() {
+    this.inputRef.current.focus()
+   }
+
+   render() {
+     return (
+      <React.Fragment>
+        <input ref={this.inputRef} type="text"></input>
+      </React.Fragment>
+     )
+   }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>hello world</h1>);
+root.render(<Input />);
