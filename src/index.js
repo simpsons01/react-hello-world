14a1172d079d4b4a6acdf31ccce52f97f8e2a711
simpsons01 <ilikekobe01@gmail.com>
2022-04-26 23:27:19 +0800

    temp

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+function Foo(props) {
+  console.log('get excuted')
+  const [count, setCount] = React.useState(0)
+  const [count2, setCount2] = React.useState(0)
+
+  const callback = React.useCallback(
+    () => {
+      console.log(`count2 is ${count2} when count2 get updated`)
+    }, 
+    [ count2 ]
+  )
+
+  const doubleCount2 = React.useMemo(() => {
+    console.log('excute double')
+    return count2 * 2
+  }, [count2])
+
+  React.useEffect(() => {
+    callback()
+  })
+
+  const add = () => {
+    setCount(prevCount => prevCount + 1)
+    if(count % 2 === 0) {
+      setCount2(prevCount2 => prevCount2 + 1)
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <button onClick={add}>
+        ++
+      </button>
+      <div>{count}</div>
+      <div>{count2}</div>
+      <div>{doubleCount2}</div>
+    </React.Fragment>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>hello world</h1>);
+root.render(<Foo />);
