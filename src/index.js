699a26129728122d28845505b3a5631bdad04297
simpsons01 <ilikekobe01@gmail.com>
2022-04-27 23:58:05 +0800

    temp

--- src/index.js
+++ src/index.js
@@ -1,26 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Button() {
-  console.log("render")
-  
-   React.useEffect(() => {
-    console.log('log from useEffect')
-   })
+function wait () {
+  let i = 0
+  while(i < 10000000000) {
+    i++
+  }
+}
+// function Button() {
+//   console.log("render")
+
+//    React.useEffect(() => {
+//     console.log('log from useEffect')
+//    })
+
+//    React.useLayoutEffect(() => {
+//      console.log('log from useLayoutEffect')
+//    })
+   
+//    return (
+//      <button>click me</button>
+//    )
+// }
+
+function NonBlockRenderButton() {
+  const [state, setState] = React.useState("hello world")
+
+  React.useEffect(() => {
+    wait()
+    setState('world hello')
+  }, [])
+
+  return (
+    <button>{state}</button>
+  )
+}
+
+function BlockRenderButton() {
+   const [state, setState] = React.useState("hello world")
 
    React.useLayoutEffect(() => {
-     console.log('log from useLayoutEffect')
-   })
-   
+     wait()
+     setState('world hello')
+   }, [])
+
    return (
-     <button>click me</button>
+     <button>{state}</button>
    )
 }
 
 
 function App() {
   return (
-    <Button />
+    <React.Fragment>
+      {/* <Button /> */}
+      <NonBlockRenderButton />
+      {/* <BlockRenderButton /> */}
+    </React.Fragment>
   )
 }
 
