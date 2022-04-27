68139a88594a7b763dddae299f1ae9a536eb6d32
simpsons01 <ilikekobe01@gmail.com>
2022-04-27 23:19:49 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+function Button() {
+  console.log("render")
+  
+   React.useEffect(() => {
+    console.log('log from useEffect')
+   })
+
+   React.useLayoutEffect(() => {
+     console.log('log from useLayoutEffect')
+   })
+   
+   return (
+     <button>click me</button>
+   )
+}
+
+
+function App() {
+  return (
+    <Button />
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>hello world</h1>);
+root.render(<App />);
