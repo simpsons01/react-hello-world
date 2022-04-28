d232608e691ac6ea4b892394173e1240c0e8585e
simpsons01 <ilikekobe01@gmail.com>
2022-04-28 10:42:15 +0800

    add example

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>hello world</h1>);
+function Counter() {
+  const ref = React.useRef(0)
+  const [count, setCount] = React.useState(0)
+  const [step, setStep] = React.useState(1)
+
+  React.useEffect(() => {
+    ref.current = ref.current + 1
+    console.log('use effect callback excute')
+    console.log(ref.current)
+    const id = setInterval(() => {
+      setCount(prevCount => prevCount + step)
+    }, 1000)  
+   return () => {
+     console.log('clean effect callback excute')
+     console.log(ref.current)
+     clearInterval(id)
+   }
+  }, [step])
+
+  const onInput = function(e) {
+    const val = parseInt(e.target.value)
+    if(val == '') {
+      return setStep(1)
+    } 
+    setStep(val)
+  }
+
+  return (
+    <React.Fragment>
+      <div>{count}</div>
+      <input onInput={onInput}></input>
+    </React.Fragment>
+  )
+}
+const root = ReactDOM.createRoot(document.querySelector("#root"))
+root.render(<Counter />);
