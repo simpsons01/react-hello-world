b62c8685449a7268a1208bf31a1938845bfe160b
ilikekobe01@gmail.com <ilikekobe01@gmail.com>
2022-04-29 14:32:16 +0800

    add feature/useImperativeHandle

--- src/index.js
+++ src/index.js
@@ -1,27 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-class Counter extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      count: 0
+// eslint-disable-next-line
+const MyInput = React.forwardRef((props, ref) => {
+
+  const _ref = React.useRef(null)
+
+  const handleImperativeHandle = {
+    focus: () => {
+      _ref.current.focus()
+    },
+    compare: (parentRef) => {
+      console.log(parentRef.current === handleImperativeHandle) // true
     }
   }
+   
+  React.useImperativeHandle(ref, () => handleImperativeHandle)
 
-  add() {
-    this.setState(prevState => ({ count: prevState.count + 1 }))
-  }
+  return (
+    <input 
+      ref={inputNode => {
+        console.log("set ref!")
+        _ref.current = inputNode
+      }} 
+      type="text" 
+    />
+  )
+})
 
-  render() {
-    return (
-      <div>
-        <div>{this.state.count}</div>
-        <button onClick={this.add.bind(this)}>+</button>
-      </div>
-    )
-  }
+function App() {
+  const ref = React.useRef(null)
+  
+  React.useEffect(() => {
+    ref.current.focus()
+    ref.current.compare(ref)
+  })
+  return (
+    <MyInput 
+      ref={x => {
+        console.log(x)
+        ref.current = x
+      }}
+    />
+  )
 }
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>hello world</h1>);
+root.render(<App />);
